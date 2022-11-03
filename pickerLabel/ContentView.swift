1ab443ce64d1718e5e738d38cefe3a111bca348a
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2022-11-03 10:42:37 -0400

    Added more examples.

--- pickerLabel/ContentView.swift
+++ pickerLabel/ContentView.swift
@@ -15,10 +15,11 @@ struct MyData : Identifiable, Hashable {
     let color : Color
 }
 
+// Label won't show the SF Symbol when using pickerStyle navigationLink.
 struct ContentView: View {
     @State private var mySelection : MyData = MyData(name: "", SFname: "", color: .clear)
     
-    let allData : [MyData] = [.init(name: "Turtle", SFname: "tortoise.fill", color: .green)]
+    let allData : [MyData] = [.init(name: "Turtle", SFname: "tortoise.fill", color: .green), .init(name: "Bird", SFname: "bird.fill", color: .red)]
     
     var body: some View {
         NavigationStack {
@@ -26,9 +27,48 @@ struct ContentView: View {
                 Picker("HStack", selection: $mySelection) {
                     ForEach(allData) { datum in
                         HStack {
+                            Text(datum.name)
                             Image(systemName: datum.SFname)
                                 .foregroundColor(datum.color)
-                            Spacer()
+                        }
+                        .tag(datum as MyData)
+                    }
+                }
+                .pickerStyle(.navigationLink)
+                
+                Picker("Grid", selection: $mySelection) {
+                    ForEach(allData) { datum in
+                        Grid {
+                            GridRow {
+                                Text(datum.name)
+                                Image(systemName: datum.SFname)
+                                    .foregroundColor(datum.color)
+                            }
+                        }
+                        .tag(datum as MyData)
+                    }
+                }
+                .pickerStyle(.navigationLink)
+                
+                Picker("Label", selection: $mySelection) {
+                    ForEach(allData) { datum in
+                        Label {
+                            Text(datum.name)
+                        } icon: {
+                            Image(systemName: datum.SFname)
+                                .foregroundColor(datum.color)
+                        }
+                        .tag(datum as MyData)
+                    }
+                }
+                .pickerStyle(.navigationLink)
+                
+                Picker("LabeledContent", selection: $mySelection) {
+                    ForEach(allData) { datum in
+                        LabeledContent {
+                            Image(systemName: datum.SFname)
+                                .foregroundColor(datum.color)
+                        } label: {
                             Text(datum.name)
                         }
                         .tag(datum as MyData)
